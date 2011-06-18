d60608161553b8ef9101da32ebee76a2fe5313b1
Florian Friesdorf <flo@chaoflow.net>
2011-06-18 17:40:17 +0200

    minimal conkerorrc

---
+++ .conkerorrc/main.js
@@ -0,0 +1,3 @@
+url_completion_use_bookmarks = true;
+define_webjump("ddg", "http://duckduckgo.com/?q=%s");
+define_webjump("pypi", "http://pypi.python.org/pypi?%3Aaction=search&term=%s&submit=search");
