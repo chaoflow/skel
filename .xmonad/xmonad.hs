6448ce19574ec1af5aa56d2cdcf89180f1f9638f
Florian Friesdorf <flo@chaoflow.net>
2011-06-19 10:40:46 +0200

    xmonad.hs indent

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,12 +3,14 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run(spawnPipe)
 
 main = do 
-     xmonad $ defaultConfig {
-            modMask = mod4Mask,
-            terminal = "urxvt"
-            } `additionalKeys` [
-            ((mod4Mask .|. shiftMask, xK_e), spawn "emacsclient -c"),
-            ((mod4Mask .|. shiftMask, xK_f), spawn "firefox"),
-            ((mod4Mask .|. shiftMask, xK_k), spawn "conkeror"),
-            ((mod4Mask .|. shiftMask, xK_z), spawn "xlock")
-            ]
+  xmonad $ defaultConfig {
+    modMask = mod4Mask,
+    terminal = "urxvt"
+    } `additionalKeys` [
+    ((mod4Mask .|. shiftMask, xK_e), spawn "emacsclient -c"),
+    ((mod4Mask .|. shiftMask, xK_f), spawn "firefox"),
+    ((mod4Mask .|. shiftMask, xK_k), spawn "conkeror"),
+    ((mod4Mask .|. shiftMask, xK_z), spawn "xlock"),
+    ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s"),
+    ((0, xK_Print), spawn "scrot")
+    ]
