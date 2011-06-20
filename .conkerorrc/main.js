a09e63c94d6e3e0b137efd39ff908195620be684
Florian Friesdorf <flo@chaoflow.net>
2011-06-20 07:25:15 +0200

    conkeror bury-buffer C-c y

--- .conkerorrc/main.js
+++ .conkerorrc/main.js
@@ -1,3 +1,5 @@
 url_completion_use_bookmarks = true;
 define_webjump("ddg", "http://duckduckgo.com/?q=%s");
 define_webjump("pypi", "http://pypi.python.org/pypi?%3Aaction=search&term=%s&submit=search");
+
+define_key(default_global_keymap, "C-c y", "bury-buffer");
