65504b945928637e4680c41785dd4c0a6df6c28b
Florian Friesdorf <flo@chaoflow.net>
2011-06-20 07:25:27 +0200

    xmonad more shortcuts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -10,6 +10,9 @@ main = do
     ((mod4Mask .|. shiftMask, xK_e), spawn "emacsclient -c"),
     ((mod4Mask .|. shiftMask, xK_f), spawn "firefox"),
     ((mod4Mask .|. shiftMask, xK_k), spawn "conkeror"),
+    ((mod4Mask .|. shiftMask, xK_o), spawn "urxvt -e offlineimap"),
+    ((mod4Mask .|. shiftMask, xK_v), spawn "vlc"),
+    ((mod4Mask .|. shiftMask, xK_w), spawn "wicd-client -n"),
     ((mod4Mask .|. shiftMask, xK_z), spawn "xlock"),
     ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s"),
     ((0, xK_Print), spawn "scrot")
