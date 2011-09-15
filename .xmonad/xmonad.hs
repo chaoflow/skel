60037ef9f1aa8f99e7360e33a69653745a1c708a
Florian Friesdorf <flo@chaoflow.net>
2011-09-16 00:30:14 +0200

    xmonad changes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,19 +1,51 @@
+import qualified XMonad.StackSet as W
+import Graphics.X11.ExtraTypes.XF86
 import XMonad
+--import XMonad.Actions.Volume
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Run(spawnPipe)
 
-main = do 
+-- for some reason stalonetray floats automatically. maybe there is a
+-- way not to automatically float it. This works at least.
+doSink :: ManageHook
+doSink = doF . W.sink =<< ask
+
+manageHook' = composeAll [
+  --(className =? "Conkeror" <&&> resource =? "Addons") --> doFloat,
+  className =? "stalonetray" --> doShift "10:attic" <+> doSink,
+  className =? "Vlc" --> doFloat,
+  className =? "Xmessage" --> doFloat,
+  className =? "Wine" --> doFloat,
+  stringProperty "WM_NAME" =? "Crack Attack!" --> doFloat,
+  stringProperty "WM_NAME" =? "glxgears" --> doFloat
+  ]
+
+modMask' = mod4Mask
+workspaces' = ["1:chat","2","3","4","5","6","7","8","9","10:attic"]
+workspaceKeys' = [xK_1..xK_9] ++ [xK_0]
+
+main = do
   xmonad $ defaultConfig {
-    modMask = mod4Mask,
-    terminal = "urxvt"
-    } `additionalKeys` [
-    ((mod4Mask .|. shiftMask, xK_e), spawn "emacsclient -c"),
-    ((mod4Mask .|. shiftMask, xK_f), spawn "firefox"),
-    ((mod4Mask .|. shiftMask, xK_k), spawn "conkeror"),
-    ((mod4Mask .|. shiftMask, xK_o), spawn "urxvt -e offlineimap"),
-    ((mod4Mask .|. shiftMask, xK_v), spawn "vlc"),
-    ((mod4Mask .|. shiftMask, xK_w), spawn "wicd-client -n"),
-    ((mod4Mask .|. shiftMask, xK_z), spawn "xlock"),
-    ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s"),
-    ((0, xK_Print), spawn "scrot")
-    ]
+    manageHook = manageHook' <+> manageHook defaultConfig,
+    modMask = modMask',
+    terminal = "urxvt",
+    workspaces = workspaces'
+    } `additionalKeys` ([
+    ((modMask' .|. shiftMask, xK_e), spawn "emacsclient -c"),
+    ((modMask' .|. shiftMask, xK_f), spawn "firefox"),
+    ((modMask' .|. shiftMask, xK_k), spawn "conkeror"),
+    ((modMask' .|. shiftMask, xK_m), spawn "urxvt -e aumix"),
+    ((modMask' .|. shiftMask, xK_o), spawn "urxvt -e offlineimap"),
+    ((modMask' .|. shiftMask, xK_r), spawn "chrome"),
+    ((modMask' .|. shiftMask, xK_t), spawn "urxvt -e ssh tesla"),
+    ((modMask' .|. shiftMask, xK_v), spawn "vlc"),
+    ((modMask' .|. shiftMask, xK_w), spawn "wicd-client -n"),
+    ((modMask' .|. shiftMask, xK_z), spawn "xlock"),
+    ((0, xK_Print), spawn "scrot -e 'mv $f ~/shots/'"),
+    ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s -e 'mv $f ~/shots/'")
+  --  ((0, xF86XK_AudioLowerVolume), lowerVolume 2 >>= alert),
+  --  ((0, xF86XK_AudioRaiseVolume), raiseVolume 2 >>= alert)
+    ] ++ [((m .|. modMask', k), windows $ f i)
+         | (i, k) <- zip workspaces' workspaceKeys'
+         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+         ])
