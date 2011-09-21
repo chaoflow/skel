ed8f0d1fabc2064f0fce7e35dc5633ca4a05a3ab
Florian Friesdorf <flo@chaoflow.net>
2011-09-21 23:05:29 +0200

    ipdb pulls in ipython, fix typo

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -9,7 +9,7 @@
         pkgs.libxml2
         pkgs.libxslt
         pkgs.python26
-        pkgs.python26Packages.ipython
+        pkgs.python26Packages.ipdb
         pkgs.python26Packages.readline
         pkgs.python26Packages.sqlite3
         pkgs.python26Packages.ssl
@@ -24,7 +24,7 @@
         pkgs.libxml2
         pkgs.libxslt
         pkgs.python27
-        pkgs.python26Packages.ipython
+        pkgs.python27Packages.ipdb
         pkgs.python27Packages.readline
         pkgs.python27Packages.sqlite3
         pkgs.python27Packages.ssl
