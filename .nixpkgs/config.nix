4159a37f52f20835e93c15fbc36d173960b36a2f
Florian Friesdorf <flo@chaoflow.net>
2011-09-21 22:38:12 +0200

    optimise py2xenv

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -9,10 +9,10 @@
         pkgs.libxml2
         pkgs.libxslt
         pkgs.python26
+        pkgs.python26Packages.ipython
         pkgs.python26Packages.readline
         pkgs.python26Packages.sqlite3
         pkgs.python26Packages.ssl
-        pkgs.python26Packages.setuptools
         pkgs.subversionClient
       ];
     };
@@ -24,12 +24,21 @@
         pkgs.libxml2
         pkgs.libxslt
         pkgs.python27
+        pkgs.python26Packages.ipython
         pkgs.python27Packages.readline
         pkgs.python27Packages.sqlite3
         pkgs.python27Packages.ssl
-        pkgs.python27Packages.setuptools
         pkgs.subversionClient
         pkgs.zlib
+      ];
+    };
+    TestEnv = pkgs.buildEnv {
+      name = "testenv";
+      paths = [
+        pkgs.defaultStdenv
+        pkgs.python26
+        pkgs.python26Packages.setuptools
+        pkgs.python26Packages.ipython
       ];
     };
   };
